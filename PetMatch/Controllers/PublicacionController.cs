c3a65673fbef723ff3d65d34ca5452fe92d189dd
EdgarFuentes100 <edgarfuentes139@gmail.com>
2025-06-24 17:53:43 -0700

    Cambio en el diseño y el controller para trear los datos de solicitudes y publicaciones

--- PetMatch/Controllers/PublicacionController.cs
+++ PetMatch/Controllers/PublicacionController.cs
@@ -1,14 +1,41 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PetMatch.Context;
+using PetMatch.Models;
+using System.Security.Claims;
 
 namespace PetMatch.Controllers
 {
     public class PublicacionController : Controller
     {
+        private readonly AppDbContext _context;
+        public PublicacionController(AppDbContext appDbContext) 
+        {
+            _context = appDbContext;
+        }
         // GET: PublicacionController
         public async Task<IActionResult> MisPublicaciones()
         {
-            return View();
+            var email = User.FindFirst(ClaimTypes.Email)?.Value;
+
+            var publicaciones = await _context.Publicaciones
+                .Include(p => p.Mascota)
+                .ThenInclude(m => m.TipoMascota)
+                .Include(p => p.Publicador)
+                .Where(p => p.Publicador.Email == email)
+                .ToListAsync();
+
+            var mascotasIds = publicaciones.Select(p => p.MascotaId).ToList();
+
+            var solicitudes = await _context.Solicitud
+                .Include(s => s.Solicitante)
+                .Where(s => mascotasIds.Contains(s.MascotaId))
+                .ToListAsync();
+
+            ViewBag.Solicitudes = solicitudes;
+
+            return View(publicaciones);
         }
 
         public async Task<IActionResult> HacerPublicacion()
