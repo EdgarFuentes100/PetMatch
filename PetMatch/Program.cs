f35e5d7726d58258b374f5e2576a26b2bca1ef0c
EdgarFuentes100 <edgarfuentes139@gmail.com>
2025-06-28 09:22:25 -0700

    Se comento una linea de codigo, y se agrgeo un app.Use

--- PetMatch/Program.cs
+++ PetMatch/Program.cs
@@ -143,11 +143,13 @@ builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(conn
 
 // Middleware
 var app = builder.Build();
-if (!app.Environment.IsDevelopment())
+app.UseDeveloperExceptionPage();
+
+/*if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
     app.UseHsts();
-}
+}^*/
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 app.UseRouting();
