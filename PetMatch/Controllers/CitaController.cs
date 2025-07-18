bac5ca0f77e95745e8864944534903e6b774fda4
EdgarFuentes100 <edgarfuentes139@gmail.com>
2025-07-18 11:57:03 -0700

    Vista de citas, datos fijos

---
+++ PetMatch/Controllers/CitaController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PetMatch.Controllers
+{
+    public class CitaController : Controller
+    {
+        // GET: CitaController
+        public async Task<IActionResult> VistaCita()
+        {
+            return View();
+        }
+    }
+}
